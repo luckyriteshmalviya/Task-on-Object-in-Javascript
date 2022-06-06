df6c884b76cdea536fd92fe7669fc11c6d68c514
Ritesh Malviya <luckyriteshmalviya@gmail.com>
2022-06-06 22:32:25 +0530

    updated other approaches

--- main.js
+++ main.js
@@ -10,34 +10,37 @@
  * and make a new key in res with the value of orderDetails.
  */
 
+const users = [
+  { id: 123, name: "Sagar", city: "Indore" },
+  { id: 124, name: "Arun", city: "Morena" },
+  { id: 125, name: "Nikhil", city: "Dhule" },
+];
 
-
-const users = [ {id: 123, name: "Sagar", city: "Indore",} ,  {id: 124, name: "Arun", city: "Morena",} , 
-               {id: 125, name: "Nikhil", city: "Dhule",} ];
-
-const orders = [ { id: 1234, name: "iPhone 13 Pro Max",    price: "123000",  customerId: 123, } ,  
-                          { id: 1235, name: "Apple iWatch", price: "49000",  customerId: 124, }, ];
+const orders = [
+  { id: 1234, name: "iPhone 13 Pro Max", price: "123000", customerId: 123 },
+  { id: 1235, name: "Apple iWatch", price: "49000", customerId: 124 },
+];
 
 // const result = [ {userId: 123, userName: "Sagar",
 //                  orders: [{orderId: 1234, productName: "iPhone 13 Pro Max",  productPrice: "123000",},],}  ,
 
-//                 {userId: 124, userName: "Arun", 
+//                 {userId: 124, userName: "Arun",
 //                 orders:  [{orderId: 1235, productName: "Apple iWatch", productPrice: "49000", },],}  ,
 
 //                 {userId: 125, userName: "Nikhil", orders: [],},
 //                 ];
 
-// const {id,  name} = users;
-// const {customerId, price} = orders;
-for(let i=0; i<users.length; i++){
 const res = [];
-const orderDetails = [];
-console.log("value of i: ",i)
-res.push( {userId:users[i].id , userName:users[i].name} )
+for (let i = 0; i < users.length; i++) {
+  const newObj = { userId: users[i].id, userName: users[i].name };
 
-if ( users[i].id === orders[i].customerId){
-         orderDetails.push( {orderId:orders[i].id, productName:orders[i].name, productPrice: orders[i].price})
-         res.order=orderDetails;
-    }    
-    console.log(res);
+  if (orders[i] && users[i].id === orders[i].customerId) {
+    newObj.order = {
+      orderId: orders[i].id,
+      productName: orders[i].name,
+      productPrice: orders[i].price,
+    };
+  } else newObj.order = [];
+  res.push(newObj);
 }
+console.log(res);
